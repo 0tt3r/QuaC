ff901c76cf24fae6d07a9d1e068ec047966570cc
Matt Otten <mjo98@cornell.edu>
2017-01-21 14:19:35 -0600

    Updated function prototypes for new kron routines.

--- src/kron_p.h
+++ src/kron_p.h
@@ -5,16 +5,16 @@
 
 long   _get_loop_limit(op_type,int);
 double _get_val_in_subspace(long,op_type,int,long*,long*);
-void   _add_to_PETSc_kron(PetscScalar,int,int,op_type,int,int,int);
-void   _add_to_PETSc_kron_comb(PetscScalar,int,int,op_type,int,int,int,
+void   _add_to_PETSc_kron(Mat,PetscScalar,int,int,op_type,int,int,int);
+void   _add_to_PETSc_kron_comb(Mat,PetscScalar,int,int,op_type,int,int,int,
                              op_type,int,int,int,int);
-void   _add_to_PETSc_kron_lin(PetscScalar,int,int,op_type,int,int,int);
-void   _add_to_PETSc_kron_lin_comb(PetscScalar,int,int,op_type,int);
-void   _add_to_PETSc_kron_ij(PetscScalar,int,int,int,int,int);
-void _add_to_PETSc_kron_comb_vec(PetscScalar,int,int,op_type,int,int,int,int,
+void   _add_to_PETSc_kron_lin(Mat,PetscScalar,int,int,op_type,int,int,int);
+void   _add_to_PETSc_kron_lin_comb(Mat,PetscScalar,int,int,op_type,int);
+void   _add_to_PETSc_kron_ij(Mat,PetscScalar,int,int,int,int,int);
+void _add_to_PETSc_kron_comb_vec(Mat,PetscScalar,int,int,op_type,int,int,int,int,
                                  int,int,int);
-void _add_to_PETSc_kron_lin2(PetscScalar,int,int,int,int);
-void _add_to_PETSc_kron_lin2_comb(PetscScalar,int,int);
+void _add_to_PETSc_kron_lin2(Mat,PetscScalar,int,int,int,int);
+void _add_to_PETSc_kron_lin2_comb(Mat,PetscScalar,int,int);
 
 void _add_to_dense_kron(double,int,int,op_type,int);
 void _add_to_dense_kron_comb(double,int,int,op_type,int,int,int,
