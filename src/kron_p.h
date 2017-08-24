35ac5373eca062f93a521f51acd5d8d03bc6554a
Matt Otten <mjo98@cornell.edu>
2017-08-24 07:26:46 -0500

    Fixed function prototypes for dense ham.

--- src/kron_p.h
+++ src/kron_p.h
@@ -16,11 +16,11 @@ void _add_to_PETSc_kron_comb_vec(Mat,PetscScalar,int,int,op_type,int,int,int,int
 void _add_to_PETSc_kron_lin2(Mat,PetscScalar,int,int,int,int);
 void _add_to_PETSc_kron_lin2_comb(Mat,PetscScalar,int,int);
 
-void _add_to_dense_kron(double,int,int,op_type,int);
-void _add_to_dense_kron_comb(double,int,int,op_type,int,int,int,
+void _add_to_dense_kron(PetscScalar,int,int,op_type,int);
+void _add_to_dense_kron_comb(PetscScalar,int,int,op_type,int,int,int,
                                op_type,int);
-void _add_to_dense_kron_comb_vec(double,int,int,op_type,int,int,int,int);
-void _add_to_dense_kron_ij(double,int,int,int,int,int);
+void _add_to_dense_kron_comb_vec(PetscScalar,int,int,op_type,int,int,int,int);
+void _add_to_dense_kron_ij(PetscScalar,int,int,int,int,int);
 void _add_PETSc_DM_kron_ij(PetscScalar,Mat,Mat,int,int,int,int,int);
 void _mult_PETSc_init_DM(Mat,Mat,double);
 
