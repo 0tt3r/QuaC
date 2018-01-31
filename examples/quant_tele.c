18ef9de647e4136281263ede590f9f64a1a4f3d7
Matt Otten <mjo98@cornell.edu>
2018-01-31 15:38:45 -0600

    Streamlined quant_tele example

--- examples/quant_tele.c
+++ examples/quant_tele.c
@@ -17,7 +17,9 @@ FILE *f_pop;
 
 
 int main(int argc,char **args){
-  PetscReal time_max,dt,*gamma_1,*omega,gate_time_step;
+  PetscReal time_max,dt,*gamma_1,*gamma_2,*omega,*sigma_x,*sigma_y,*sigma_z;
+  PetscReal gate_time_step,theta,fidelity;
+  PetscScalar mat_val;
   PetscInt  steps_max;
   Vec rho;
   int num_qubits,i,j,h_dim,system;
@@ -28,33 +30,69 @@ int main(int argc,char **args){
   num_qubits = 3;
   qubits  = malloc(num_qubits*sizeof(struct operator)); //Only need 3 qubits for teleportation
   gamma_1 = malloc(num_qubits*sizeof(PetscReal)); //Only need 3 qubits for teleportation
+  gamma_2 = malloc(num_qubits*sizeof(PetscReal)); //Only need 3 qubits for teleportation
+  sigma_x = malloc(num_qubits*sizeof(PetscReal)); //Only need 3 qubits for teleportation
+  sigma_y = malloc(num_qubits*sizeof(PetscReal)); //Only need 3 qubits for teleportation
+  sigma_z = malloc(num_qubits*sizeof(PetscReal)); //Only need 3 qubits for teleportation
   omega   = malloc(num_qubits*sizeof(PetscReal)); //Only need 3 qubits for teleportation
   for (i=0;i<num_qubits;i++){
     create_op(2,&qubits[i]);
     omega[i]   = 0.0;
     gamma_1[i] = 0;
+    gamma_2[i] = 0;
+    sigma_x[i] = 0;
+    sigma_y[i] = 0;
+    sigma_z[i] = 0;
   }
 
+  theta = 0.0;
   PetscOptionsGetReal(NULL,NULL,"-gam1",&gamma_1[0],NULL);
   PetscOptionsGetReal(NULL,NULL,"-gam2",&gamma_1[1],NULL);
   PetscOptionsGetReal(NULL,NULL,"-gam3",&gamma_1[2],NULL);
+
+  PetscOptionsGetReal(NULL,NULL,"-dep1",&gamma_2[0],NULL);
+  PetscOptionsGetReal(NULL,NULL,"-dep2",&gamma_2[1],NULL);
+  PetscOptionsGetReal(NULL,NULL,"-dep3",&gamma_2[2],NULL);
+
+  PetscOptionsGetReal(NULL,NULL,"-sigx1",&sigma_x[0],NULL);
+  PetscOptionsGetReal(NULL,NULL,"-sigx2",&sigma_x[1],NULL);
+  PetscOptionsGetReal(NULL,NULL,"-sigx3",&sigma_x[2],NULL);
+
+  PetscOptionsGetReal(NULL,NULL,"-sigy1",&sigma_y[0],NULL);
+  PetscOptionsGetReal(NULL,NULL,"-sigy2",&sigma_y[1],NULL);
+  PetscOptionsGetReal(NULL,NULL,"-sigy3",&sigma_y[2],NULL);
+
+  PetscOptionsGetReal(NULL,NULL,"-sigz1",&sigma_z[0],NULL);
+  PetscOptionsGetReal(NULL,NULL,"-sigz2",&sigma_z[1],NULL);
+  PetscOptionsGetReal(NULL,NULL,"-sigz3",&sigma_z[2],NULL);
+
   PetscOptionsGetReal(NULL,NULL,"-om1",&omega[0],NULL);
   PetscOptionsGetReal(NULL,NULL,"-om2",&omega[1],NULL);
   PetscOptionsGetReal(NULL,NULL,"-om3",&omega[2],NULL);
 
+  PetscOptionsGetReal(NULL,NULL,"-theta",&theta,NULL);
 
   /* Add terms to the hamiltonian */
   for (i=0;i<num_qubits;i++){
     add_to_ham(omega[i],qubits[i]->n);
     /* qubit decay */
     add_lin(gamma_1[i],qubits[i]);
+    add_lin(gamma_2[i],qubits[i]->n);
+    add_lin(sigma_x[i],qubits[i]->sig_x);
+    add_lin(sigma_y[i],qubits[i]->sig_y);
+    add_lin(sigma_z[i],qubits[i]->sig_z);
   }
 
 
   printf("gam: %f %f %f\n",gamma_1[0],gamma_1[1],gamma_1[2]);
+  printf("dep: %f %f %f\n",gamma_2[0],gamma_2[1],gamma_2[2]);
+  printf("sigx: %f %f %f\n",sigma_x[0],sigma_x[1],sigma_x[2]);
+  printf("sigy: %f %f %f\n",sigma_y[0],sigma_y[1],sigma_y[2]);
+  printf("sigz: %f %f %f\n",sigma_z[0],sigma_z[1],sigma_z[2]);
   printf("om: %f %f %f\n",omega[0],omega[1],omega[2]);
+  printf("theta: %f\n",theta);
 
-  time_max  = 20;
+  time_max  = 9;
   dt        = 0.01;
   steps_max = 1000;
 
@@ -68,74 +106,64 @@ int main(int argc,char **args){
 
   create_full_dm(&rho);
 
-  set_initial_pop(qubits[0],1);
-  //  set_initial_pop(qubits[1],1);
-  //  set_initial_pop(qubits[2],1);
-  set_dm_from_initial_pop(rho);
-
-
   create_dm(&rho_init,2);
   create_dm(&rho_tmp,2);
 
+  //Set the initial DM
+  mat_val = cos(theta)*cos(theta);
+  mat_val = 0.5;
+  add_value_to_dm(rho,0,0,mat_val);
+  /* mat_val = cos(theta)*sin(theta); */
+  mat_val = -0.5;
+  add_value_to_dm(rho,4,0,mat_val);
+  add_value_to_dm(rho,0,4,mat_val);
+  /* mat_val = sin(theta)*sin(theta); */
+  mat_val = 0.5;
+  add_value_to_dm(rho,4,4,mat_val);
+  assemble_dm(rho);
 
-
+  print_dm(rho,8);
   h_dim = pow(2,num_qubits);
 
-  system = 0;
-  _apply_gate(HADAMARD,&system,rho); //Hadamard on 0 for superposed starting state
-
-
   partial_trace_over(rho,rho_init,2,qubits[1],qubits[2]);
-
-  //  print_dm(rho,hdim); //Print starting state
-  //  systems[0] = 1;
-  //  systems[1] = 2;
-
-  //  _apply_gate(HADAMARD,systems,rho); //Hadamard on 1
-  //  print_dm(rho,h_dim);
-
-  //  _apply_gate(CNOT,systems,rho); //CNOT
-  //  print_dm(rho,h_dim);
-
-  //  systems[0]=0; systems[1]=1;
-  //  _apply_gate(CNOT,systems,rho); //CNOT
-  //  print_dm(rho,h_dim);
-
-  //  _apply_gate(HADAMARD,systems,rho); //Hadamard on 0
-  //  print_dm(rho,h_dim);
-
-  //  systems[0]=1; systems[1]=2;
-  //  _apply_gate(CNOT,systems,rho); //CNOT
-  //  print_dm(rho,h_dim);
-
-  //  systems[0] = 2;
-  //  _apply_gate(HADAMARD,systems,rho); //Hadamard on 2
-  //  print_dm(rho,h_dim);
-
-  //  systems[0]=0; systems[1]=2;
-  //  _apply_gate(CNOT,systems,rho); //CNOT
-  //  print_dm(rho,h_dim);
-
-  //  systems[0] = 2;
-  //  _apply_gate(HADAMARD,systems,rho); //Hadamard on 2
-  //  print_dm(rho,h_dim);
-
+  // 1 = Identity
+  // 2 = H on A
+  // 3 = H on B
+  // 4 = H on C
+  // 5 = CNOT(A,B) [A control]
+  // 6 = CNOT(B,C) [B control]
+  // 7 = CNOT(A,C) [A control]
   gate_time_step = 1.0;
   add_gate(1*gate_time_step,HADAMARD,1); //Hadamard on 1
   add_gate(2*gate_time_step,CNOT,1,2); //CNOT(1,2)
   add_gate(3*gate_time_step,CNOT,0,1); //CNOT(0,1)
   add_gate(4*gate_time_step,HADAMARD,0); //Hadamard on 0
-  add_gate(5*gate_time_step,CNOT,1,2); //CNOT(1,2)
-  add_gate(6*gate_time_step,HADAMARD,2); //Hadamard on 2
-  add_gate(7*gate_time_step,CNOT,0,2); //CNOT(0,2)
-  add_gate(8*gate_time_step,HADAMARD,2); //Hadamard on 2
+  /* add_gate(5*gate_time_step,CNOT,1,2); //CNOT(1,2) */
+  /* add_gate(6*gate_time_step,HADAMARD,2); //Hadamard on 2 */
+  /* add_gate(7*gate_time_step,CNOT,0,2); //CNOT(0,2) */
+  /* add_gate(8*gate_time_step,HADAMARD,2); //Hadamard on 2 */
 
-  time_step(rho,time_max,dt,steps_max);
+  add_gate(5*gate_time_step,HADAMARD,2); //Hadamard on 2
+  add_gate(6*gate_time_step,CNOT,0,2); //CNOT(0,2)
+  add_gate(7*gate_time_step,HADAMARD,2); //Hadamard on 2
+  add_gate(8*gate_time_step,CNOT,1,2); //CNOT(1,2)
+   time_step(rho,time_max,dt,steps_max);
   //  steady_state(rho);
+  partial_trace_over(rho,rho_tmp,2,qubits[0],qubits[1]);
+  get_fidelity(rho_init,rho_tmp,&fidelity);
+  if(nid==0) printf("Final fidelity: %f\n",fidelity);
 
   //  partial_trace_over(rho,rho_init,2,qubits[0],qubits[1]);
   print_dm(rho,h_dim);
-  //  print_dm(rho_init,2);
+  if(nid==0) printf("Final PTraced DM: C\n");
+  print_dm(rho_tmp,2);
+  if(nid==0) printf("Final PTraced DM: A\n");
+  partial_trace_over(rho,rho_tmp,2,qubits[1],qubits[2]);
+  print_dm(rho_tmp,2);
+  if(nid==0) printf("Final PTraced DM: B\n");
+  partial_trace_over(rho,rho_tmp,2,qubits[0],qubits[2]);
+  print_dm(rho_tmp,2);
+
   destroy_dm(rho_init);
   destroy_dm(rho_tmp);
   for (i=0;i<num_qubits;i++){
