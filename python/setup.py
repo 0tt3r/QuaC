98f48869f13cc80b33d3fcf95017e51cf54794c9
Hal Finkel <hfinkel@anl.gov>
2018-12-30 22:01:21 +0000

    Add python ext which can initialize

---
+++ python/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup, Extension
+import os
+
+module1 = Extension('quac',
+                    include_dirs = [ os.environ['PETSC_DIR'] + '/include',
+                                     os.environ['PETSC_DIR'] + '/' + os.environ['PETSC_ARCH'] + '/include',
+                                     os.getcwd() + '/../src' ],
+                    library_dirs = [ os.environ['PETSC_DIR'] + '/' + os.environ['PETSC_ARCH'] + '/lib',
+                                     os.getcwd() + '/..' ],
+                    libraries = [ 'quac', 'petsc' ],
+                    sources = ['toolkit.c'])
+
+setup (name = 'QuaC',
+       version = '1.0',
+       description = 'QuaC: Time-Dependent Open-Quantum-Systems Solver',
+       author = 'Matthew Otten, et al.',
+       author_email = 'otten@anl.gov',
+       url = 'https://github.com/0tt3r/QuaC',
+       ext_modules = [module1])
+
---
+++ python/test.py
@@ -0,0 +1,3 @@
+import quac
+quac.initialize()
+
