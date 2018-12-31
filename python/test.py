1ff18f8e24e52a88ce3c64ee3fae270660f03d02
Hal Finkel <hfinkel@anl.gov>
2018-12-31 21:59:45 +0000

    add_gate and other fixes

--- python/test.py
+++ python/test.py
@@ -8,7 +8,12 @@ print("num_nodes: {0:d}".format(q.num_nodes))
 
 c = quac.Circuit()
 print(c)
-c.initialize(25);
+c.initialize(25)
+print(c)
+c.add_gate(gate="x", qubit1=0)
+c.add_gate(gate="rz", qubit1=0, angle=0.447, time=0.5)
+c.add_gate(gate="y", qubit1=1, time=2.5)
+c.add_gate(gate="cnot", qubit1=0, qubit2=1, time=3)
 print(c)
 
 quac.finalize()
